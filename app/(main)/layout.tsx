0d30b97e7583d400ad0422c8cdf8f2df2dd9fed8
drewsephski <drewsepeczi@gmail.com>
2025-08-25 21:08:34 -0500

    remove mouse tracker

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,6 +1,5 @@
 import Providers from "@/app/(main)/providers";
 import { Toaster } from "@/components/ui/toaster";
-import MouseTracker from "@/components/mouse-tracker";
 
 export default function Layout({
   children,
@@ -10,7 +9,6 @@ export default function Layout({
   return (
     <Providers>
       <body className="flex min-h-full flex-col bg-gray-100 text-gray-900 antialiased dark:bg-gray-900 dark:text-gray-100">
-        <MouseTracker />
         {children}
 
         <Toaster />
